cfa680a782d1e6acfbb6089dbf744ab478b54b34
DSWE <dswe@mboutrecht.nl>
2020-11-11 10:49:24 +0100

    Player movement en animatie toegevoegd. Cinemachine wordt gebruikt om de camera de speler te laten volgen.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    [SerializeField] private float walkSpeed = 2f;
+    [SerializeField] private float turnSpeed = 15f;
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Walking();
+        Turning();
+    }
+
+    void Walking()
+    {
+        float vertical = Input.GetAxisRaw("Vertical");
+        if (vertical != 0)
+        {
+            animator.SetBool("walking", true);
+        }
+        else
+        {
+            animator.SetBool("walking", false);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            animator.SetBool("running", true);
+            walkSpeed = 7f;
+        }
+        else
+        {
+            animator.SetBool("running", false);
+            walkSpeed = 2f;
+        }
+        transform.Translate(vertical * Time.deltaTime * walkSpeed, 0f, 0f);
+    }
+
+    void Turning()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
         
+        transform.Rotate(0,0,-horizontal * Time.deltaTime * turnSpeed);
     }
 }
